9bd3ba723650702b6c66f29ecf311d86de1f7569
mlap <mlap@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-06-30 09:51:31 +0000

    change summary displ_coef to displ_coefs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/uibk-rprog-2017@168 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/nndd/R/nndd.R
+++ pkg/nndd/R/nndd.R
@@ -890,118 +890,29 @@ cluster.vcov_nndd <- function(model, cluster)
   
 }
 
-summary.lm <- function (object, correlation = FALSE, symbolic.cor = FALSE, use_displcoef = TRUE, ...) 
+summary.lm <- function (object, correlation = FALSE, symbolic.cor = FALSE, use_displcoef = TRUE,
+                         ...) 
 {
-  z <- object
-p <- z$rank
-rdf <- z$df.residual
-if (p == 0) {
-  r <- z$residuals
-  n <- length(r)
-  w <- z$weights
-  if (is.null(w)) {
-    rss <- sum(r^2)
-  }
-  else {
-    rss <- sum(w * r^2)
-    r <- sqrt(w) * r
-  }
-  resvar <- rss/rdf
-  ans <- z[c("call", "terms", if (!is.null(z$weights)) "weights")]
-  class(ans) <- "summary.lm"
-  ans$aliased <- is.na(coef(object))
-  ans$residuals <- r
-  ans$df <- c(0L, n, length(ans$aliased))
-  ans$coefficients <- matrix(NA, 0L, 4L)
-  dimnames(ans$coefficients) <- list(NULL, c("Estimate", 
-                                             "Std. Error", "t value", "Pr(>|t|)"))
-  ans$sigma <- sqrt(resvar)
-  ans$r.squared <- ans$adj.r.squared <- 0
-  return(ans)
-}
-if (is.null(z$terms)) 
-  stop("invalid 'lm' object:  no 'terms' component")
-if (!inherits(object, "lm")) 
-  warning("calling summary.lm(<fake-lm-object>) ...")
-Qr <- stats:::qr.lm(object)
-n <- NROW(Qr$qr)
-if (is.na(z$df.residual) || n - p != z$df.residual) 
-  warning("residual degrees of freedom in object suggest this is not an \"lm\" fit")
-r <- z$residuals
-f <- z$fitted.values
-w <- z$weights
-if (is.null(w)) {
-  mss <- if (attr(z$terms, "intercept")) 
-    sum((f - mean(f))^2)
-  else sum(f^2)
-  rss <- sum(r^2)
-}
-else {
-  mss <- if (attr(z$terms, "intercept")) {
-    m <- sum(w * f/sum(w))
-    sum(w * (f - m)^2)
-  }
-  else sum(w * f^2)
-  rss <- sum(w * r^2)
-  r <- sqrt(w) * r
-}
-resvar <- rss/rdf
-if (is.finite(resvar) && resvar < (mean(f)^2 + var(f)) * 
-    1e-30) 
-  warning("essentially perfect fit: summary may be unreliable")
-p1 <- 1L:p
-R <- chol2inv(Qr$qr[p1, p1, drop = FALSE])
-se <- sqrt(diag(R) * resvar)
-est <- z$coefficients[Qr$pivot[p1]]
-tval <- est/se
-ans <- z[c("call", "terms", if (!is.null(z$weights)) "weights")]
-ans$residuals <- r
-ans$coefficients <- cbind(est, se, tval, 2 * pt(abs(tval), 
-                                                rdf, lower.tail = FALSE))
-dimnames(ans$coefficients) <- list(names(z$coefficients)[Qr$pivot[p1]], 
-                                   c("Estimate", "Std. Error", "t value", "Pr(>|t|)"))
-ans$aliased <- is.na(coef(object))
-ans$sigma <- sqrt(resvar)
-ans$df <- c(p, rdf, NCOL(Qr$qr))
-if (p != attr(z$terms, "intercept")) {
-  df.int <- if (attr(z$terms, "intercept")) 
-    1L
-  else 0L
-  ans$r.squared <- mss/(mss + rss)
-  ans$adj.r.squared <- 1 - (1 - ans$r.squared) * ((n - 
-                                                     df.int)/rdf)
-  ans$fstatistic <- c(value = (mss/(p - df.int))/resvar, 
-                      numdf = p - df.int, dendf = rdf)
-}
-else ans$r.squared <- ans$adj.r.squared <- 0
-ans$cov.unscaled <- R
-dimnames(ans$cov.unscaled) <- dimnames(ans$coefficients)[c(1, 
-                                                           1)]
-if (correlation) {
-  ans$correlation <- (R * resvar)/outer(se, se)
-  dimnames(ans$correlation) <- dimnames(ans$cov.unscaled)
-  ans$symbolic.cor <- symbolic.cor
-}
-if (!is.null(z$na.action)) 
-  ans$na.action <- z$na.action
-class(ans) <- "summary.lm"
-
-if(use_displcoef)
-{
-if(length(object$displ_coef) > 0)
-{
-  ans$coefficients <- as.matrix(ans$coefficients[which(rownames(ans$coefficients) %in% object$displ_coef),])
   
-  if(ncol(ans$coefficients) == 1)
+    s1 <- stats:::summary.lm(object, ...)
+  
+
+  if(use_displcoef)
   {
-    ans$coefficients <- t(ans$coefficients)
-  }
+    if(length(object$displ_coefs) > 0)
+    {
+      s1$coefficients <- as.matrix(s1$coefficients[which(rownames(s1$coefficients) %in% object$displ_coefs),])
+      if(ncol(s1$coefficients) == 1)
+      {
+        s1$coefficients <- t(s1$coefficients)
+      }
+      rownames(s1$coefficients) <- object$displ_coefs
+      #colnames(s1$coefficients) <- c( "est", "se", "stat", "p", "lwr", "upr")
+    }
     
-  rownames(ans$coefficients) <- object$displ_coef
-  #colnames(s1$coefficients) <- c( "est", "se", "stat", "p", "lwr", "upr")
-}
-}
-ans
+  }
+  return(s1)
+  
 }
   
 
@@ -1025,14 +936,14 @@ summary.lmc <- function (object, correlation = FALSE, symbolic.cor = FALSE, use_
   }
   if(use_displcoef)
   {
-  if(length(object$displ_coef) > 0)
+  if(length(object$displ_coefs) > 0)
   {
-    s1$coefficients <- as.matrix(s1$coefficients[which(rownames(s1$coefficients) %in% object$displ_coef),])
+    s1$coefficients <- as.matrix(s1$coefficients[which(rownames(s1$coefficients) %in% object$displ_coefs),])
     if(ncol(s1$coefficients) == 1)
     {
       s1$coefficients <- t(s1$coefficients)
     }
-    rownames(s1$coefficients) <- object$displ_coef
+    rownames(s1$coefficients) <- object$displ_coefs
     #colnames(s1$coefficients) <- c( "est", "se", "stat", "p", "lwr", "upr")
   }
     
@@ -1060,14 +971,14 @@ summary.nndd <- function (object, correlation = FALSE, symbolic.cor = FALSE, use
   }
   if(use_displcoef)
   {
-    if(length(object$displ_coef) > 0)
+    if(length(object$displ_coefs) > 0)
     {
-      s1$coefficients <- as.matrix(s1$coefficients[which(rownames(s1$coefficients) %in% object$displ_coef),])
+      s1$coefficients <- as.matrix(s1$coefficients[which(rownames(s1$coefficients) %in% object$displ_coefs),])
       if(ncol(s1$coefficients) == 1)
       {
         s1$coefficients <- t(s1$coefficients)
       }
-      rownames(s1$coefficients) <- object$displ_coef
+      rownames(s1$coefficients) <- object$displ_coefs
       #colnames(s1$coefficients) <- c( "est", "se", "stat", "p", "lwr", "upr")
     }
   }
@@ -1154,8 +1065,8 @@ getSummary.lm <- function(obj, alpha = 0.05, report , ...)
                deviance = deviance, AIC = AIC, BIC = BIC, N = N)
   if(length(obj$displ_coef) > 0)
   {
-  coef <- as.matrix(coef[which(rownames(coef) %in% obj$displ_coef),])
-  rownames(coef) <- obj$displ_coef
+  coef <- as.matrix(coef[which(rownames(coef) %in% obj$displ_coefs),])
+  rownames(coef) <- obj$displ_coefs
   colnames(coef) <- c( "est", "se", "stat", "p", "lwr", "upr")
   }
   list(coef = coef, sumstat = sumstat, contrasts = obj$contrasts, 
